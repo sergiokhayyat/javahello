21ac2905483217b73efec295c78b139d975e1574
Sergio-Nabil Khayyat <sergio.khayyat@gmail.com>
2016-05-12 10:27:56 +0200

    Updated

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -20,7 +20,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, docker!!");
+        System.out.println("Hello, docker!! updated ;)");
     }
 
 }
